9e5913da3285b96b22082fc8522c2085ca7ca882
Eliana Jacobson <eliana.s.jacobson+git@gmail.com>
2021-06-08 12:02:52 -0700

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -38,12 +38,11 @@ int main() {
 
 	//prepare simulations
 	auto topology = Topologies::square_grid(L);
-	NetworkSim<RepeaterProtocol::Fusion_Decohere<NGHZ, decltype(topology)>, decltype(topology)> sim(topology);
+	NetworkSim<RepeaterProtocol::Fusion<NGHZ, decltype(topology)>, decltype(topology)> sim(topology);
 	
 	std::set<size_t> k;
 	std::set<double> p;
 	std::set<double> q;
-	std::set<double> mu;
 
 	std::ifstream parameters;
 	parameters.open("./parameters.txt");
@@ -77,11 +76,6 @@ int main() {
 				q.insert(std::atof(token[i].c_str()));
 			}
 		}
-		else if (token[0] == "mu") {
-			for (size_t i = 1; i < token.size(); i++) {
-				mu.insert(std::atof(token[i].c_str()));
-			}
-		}
 		else if (mpiRank == 0) {
 			std::cerr << "No parameter specified in parameter file line " << i << '.' << std::endl;
 		}
@@ -99,47 +93,44 @@ int main() {
 	std::vector<std::atomic<uint32_t>> localSuccesses(p.size() * q.size());
 	std::vector<std::atomic<uint32_t>> globalSuccesses(p.size() * q.size());
 
-	for (auto mit = mu.begin(); mit != mu.end(); mit++) {
-		std::ofstream output;
-		if (mpiRank == 0) {
-			output.open("./out/results_mu=" + std::to_string(*mit) + "_L=" + std::to_string(L) + ",A=" + std::to_string(alice) + ",B=" + std::to_string(bob) + ".txt");
-			output << "k\tp\tq\tr" << std::endl;
+	std::ofstream output;
+	if (mpiRank == 0) {
+		output.open("./out/results_L=" + std::to_string(L) + ",A=" + std::to_string(alice) + ",B=" + std::to_string(bob) + ".txt");
+		output << "k\tp\tq\tr" << std::endl;
+	}
+
+	for (auto kit = k.begin(); kit != k.end(); kit++) {
+		for (int i = 0; i < localSuccesses.size(); i++) {
+			localSuccesses[i] = 0;
+		}
+		for (int i = 0; i < globalSuccesses.size(); i++) {
+			globalSuccesses[i] = 0;
 		}
 
-		for (auto kit = k.begin(); kit != k.end(); kit++) {
-			for (int i = 0; i < localSuccesses.size(); i++) {
-				localSuccesses[i] = 0;
-			}
-			for (int i = 0; i < globalSuccesses.size(); i++) {
-				globalSuccesses[i] = 0;
-			}
-
-			size_t start, end;
-			std::tie(start, end) = work_range(mpiRank, mpiWorldSize, N * localSuccesses.size());
+		size_t start, end;
+		std::tie(start, end) = work_range(mpiRank, mpiWorldSize, N * localSuccesses.size());
 
 #pragma omp parallel for
-			for (int i = start; i < end; i++) {
-				static thread_local std::mt19937 rand(labs(((labs(((long)(time(NULL) * 181l) * (((long)(omp_get_thread_num()) - 83l) * 359l)) % 104729l) * 181l) * (((long)(mpiRank)-83l) * 359l)) % 104729l));
-				std::exponential_distribution<double> decDist(1.0 / *mit);
+		for (int i = start; i < end; i++) {
+			static thread_local std::mt19937 rand(labs(((labs(((long)(time(NULL) * 181l) * (((long)(omp_get_thread_num()) - 83l) * 359l)) % 104729l) * 181l) * (((long)(mpiRank)-83l) * 359l)) % 104729l));
 
-				size_t effI = i / N;
+			size_t effI = i / N;
 
-				localSuccesses[effI] += sim(rand, *kit, params[effI].p, params[effI].q, alice, bob, decDist);
-			}
-
-			MPI_Reduce(localSuccesses.data(), globalSuccesses.data(), localSuccesses.size(), MPI_UINT32_T, MPI_SUM, 0, MPI_COMM_WORLD);
-
-			if (mpiRank == 0) {
-				for (size_t i = 0; i < globalSuccesses.size(); i++) {
-					double rate = (double)(globalSuccesses[i]) / (double)(N * *kit);
-					output << *kit << '\t' << params[i].p << '\t' << params[i].q << '\t' << rate << std::endl;
-				}
-			}
+			localSuccesses[effI] += sim(rand, *kit, params[effI].p, params[effI].q, alice, bob);
 		}
+
+		MPI_Reduce(localSuccesses.data(), globalSuccesses.data(), localSuccesses.size(), MPI_UINT32_T, MPI_SUM, 0, MPI_COMM_WORLD);
 
 		if (mpiRank == 0) {
-			output.close();
+			for (size_t i = 0; i < globalSuccesses.size(); i++) {
+				double rate = (double)(globalSuccesses[i]) / (double)(N * *kit);
+				output << *kit << '\t' << params[i].p << '\t' << params[i].q << '\t' << rate << std::endl;
+			}
 		}
+	}
+
+	if (mpiRank == 0) {
+		output.close();
 	}
 
 	MPI_Finalize();
